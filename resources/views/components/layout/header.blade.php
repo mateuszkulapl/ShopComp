39fbcc7426f7047dbedaab76a2b8b8df05f0090b
mateuszkulapl <mateuszkulapl@gmail.com>
2023-01-02 22:54:31 +0100

    header search

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,46 +1,64 @@
 @props(['widthClasses'])
-<header {{ $attributes->merge(['class' => 'bg-slate-900 mb-4 ']) }}>
+<header {{ $attributes->merge(['class' => 'bg-slate-900 mb-4']) }}>
     <div class="max-w-7xl w-full mx-auto px-2 py-2 flex justify-between">
         <div>
             <a href="{{route('group.index')}}" class="text-xl">{{ config('app.name') }}</a>
         </div>
         <div>
-            <span onclick="document.querySelector('#headerSearch').classList.toggle('hidden');" class=" cursor-pointer">Szukaj</p>
+            <span id="headerSearchTrigger" class=" cursor-pointer">Szukaj</p>
         </div>
     </div>
-    <div id="headerSearch" class="max-w-7xl w-full mx-auto px-2 py-0 hidden ">
-        <form action="{{route('group.searchPost')}}" method="post" class=" flex items-center w-full justify-center">
-            @csrf
-            <label for="search" class=" text-lg mr-4">Wyszukaj</label>
-            <input type="text" name="search" id="search" class="
-                inline-block w-96 p-1 my-1
+    <div id="headerSearch" class=" absolute w-full hidden bg-slate-900 border-t border-slate-800">
+        <div class="max-w-7xl w-full mx-auto px-2 py-2">
+            <form action="{{route('group.searchPost')}}" method="post" class=" flex items-center w-full justify-center">
+                @csrf
+                <label for="search" class=" text-lg mr-4">Wyszukaj</label>
+                <input type="text" name="search" id="search" class="
+                inline-block w-96 py-1 px-2 my-1
                 text-md
                 shadow-sm
-                bg-slate-700 focus:bg-slate-600 hover:bg-slate-600
+                bg-slate-700 hover:bg-slate-600
                 border-none
                 rounded-md
                 outline-none
-                ring-1 ring-slate-600 focus:ring-slate-400
+                ring-1 ring-slate-600
                 rounded-r-none
                 mr-0
-                "
-                placeholder="Podaj nazwę produktu" required
-                value="">
-                
-            <div id="buttons" class="flex items-center justify-center align-middle space-x-4">
-                <button type="submit" class="
-                    inline-block  p-1 my-1
+                " placeholder="Podaj nazwę produktu" required value="">
+
+                <div id="buttons" class="flex items-center justify-center align-middle space-x-4">
+                    <button type="submit" class="
+                    inline-block py-1 px-4 my-1
                     text-md
                     shadow-sm
-                    bg-slate-700 focus:bg-slate-500 hover:bg-slate-600
+                    bg-slate-700 hover:bg-slate-600 active:bg-slate-400
                     border-none
                     rounded-md
                     outline-none
-                    ring-1 ring-slate-600 focus:ring-slate-400
+                    ring-1 ring-slate-600
                     rounded-l-none
                     ml-0
                     ">Szukaj!</button>
-            </div>
-        </form>
+                </div>
+            </form>
+            <script>
+                function clickHeaderSearch() {
+                    const headerSearchTrigger = document.getElementById('headerSearchTrigger');
+                    const headerSearch = document.getElementById('headerSearch');
+                    if (headerSearchTrigger.contains(event.target)) {
+                        headerSearch.classList.remove('hidden');
+                        document.getElementById('search').focus();
+                        headerSearchTrigger.classList.add('hidden');
+                        return;
+                    }
+                    if (!headerSearch.contains(event.target)) {
+                        headerSearch.classList.add('hidden');
+                        headerSearchTrigger.classList.remove('hidden');
+                    }
+                }
+                document.addEventListener('click', clickHeaderSearch, false);
+
+            </script>
+        </div>
     </div>
 </header>
--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -4,8 +4,8 @@
     <form action="{{route('group.searchPost')}}" method="post" class="  text-center">
         @csrf
         @if($emptySearchResults)
-                <p class=" text-lg text-rose-300 font-bold mx-4">Nie znaleziono produktów</p>
-            @endif
+        <p class=" text-lg text-rose-300 font-bold mx-4">Nie znaleziono produktów</p>
+        @endif
         <label for="search" class=" text-lg">Wyszukaj{{$emptySearchResults?' ponownie' :''}}</label>
         <input type="text" name="search" id="search" class="
             block w-96 p-2 my-2 mx-auto
@@ -17,14 +17,12 @@
             outline-none
             ring-1 ring-slate-400 focus:ring-slate-200
 
-            "
-            placeholder="Podaj nazwę produktu" required {{$searchTerm? '' : ' autofocus ' }}
-            value="{{ old('search')?old('search'):$searchTerm?$searchTerm:'' }}">
-            
+            " placeholder="Podaj nazwę produktu" required {{$searchTerm? '' : ' autofocus ' }} value="{{ old('search')?old('search'):$searchTerm?$searchTerm:'' }}">
+
         <div id="buttons" class="flex items-center justify-center align-middle space-x-4">
             <button type="submit" class=" inline-block
                 px-4 py-2 m-0 text-sm shadow-sm
-                bg-slate-700 focus:bg-slate-500 hover:bg-slate-600
+                bg-slate-700 focus:bg-slate-500 hover:bg-slate-600 active:bg-slate-400
                 border-none
                 rounded-lg
                 outline-none
