9b37c1ca8e3abf4dbc712e53fee1cb38f0853563
mateuszkulapl <mateuszkulapl@gmail.com>
2023-02-12 12:54:49 +0100

    change old price validator

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -105,7 +105,7 @@ class ApiController extends Controller
                 'ean' => 'required|string',
                 'title' => 'required|string',
                 'price_current' => 'required|numeric|max:999999.99|min:0.01',
-                'price_old' => 'numeric|gt:price_current|max:999999.99|min:0.01',
+                'price_old' => 'numeric|gte:price_current|max:999999.99|min:0.01',
                 'url' => 'url',
                 'images' => 'sometimes|array|distinct',
                 'images.*' => 'sometimes|distinct',
