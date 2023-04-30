c6b24d9eab9d3435434e2d3ef9b509358c288a8c
mateuszkulapl <mateuszkulapl@gmail.com>
2023-04-30 17:17:11 +0200

    add barcode image

--- resources/views/group/show.blade.php
+++ resources/views/group/show.blade.php
@@ -1,5 +1,14 @@
 <x-layout :chart="true" :title="$group->getSeoTitle()" :appendTitleSuffix="true" :breadcumbs="$breadcumbs">
-    <h1 class="text-4xl mb-8">EAN: {{ $group->ean }}</h1>
+    <h1 class="text-4xl mb-8 inline-block">EAN: {{ $group->ean }}</h1>
+    @if (strlen($group->ean) == 13)
+        <div style="background: white; padding:4px; float:right;"><img src="https://barcode.tec-it.com/barcode.ashx?data={{ $group->ean }}&code=EAN13&translate-esc=true" alt="kod kreskowy ean {{ $group->ean }}"></div>
+        <div style="clear:both;"></div>
+    @else
+        @if (strlen($group->ean) == 8)
+            <div style="background: white; padding:4px; float:right;"><img src="https://barcode.tec-it.com/barcode.ashx?data={{ $group->ean }}&code=EAN8&translate-esc=true" alt="kod kreskowy ean {{ $group->ean }}"></div>
+            <div style="clear:both;"></div>
+        @endif
+    @endif
 
     @if ($products->isNotEmpty())
         <x-product.products-summary :products="$products" />
