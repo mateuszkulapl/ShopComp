5c470849c0f7c31552bd09c50ecec09e47da37d5
mateuszkulapl <mateuszkulapl@gmail.com>
2023-09-03 18:10:42 +0200

    fix price addition of not available products

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use stdClass;
 
 class CartController extends Controller
 {
-    public function index($eans='')
+    public function index($eans = '')
     {
         $eanCodes = explode(',', $eans);
         $groups = \App\Models\Group::whereIn('ean', $eanCodes)->orderBy('ean')->get();
@@ -17,7 +18,7 @@ class CartController extends Controller
         $groups->load('products', 'products.shop', 'oldestProduct.oldestImage', 'products.latestPrice');
 
         $shops = $groups->pluck('products')->flatten()->pluck('shop')->unique();
-        
+
         $breadcumbs = $this->getBreadcumbs();
 
         foreach ($groups as $group) {
@@ -25,11 +26,14 @@ class CartController extends Controller
             $group->minPrice = $group->products->min('latestPrice.current');
             $group->avgPrice = $group->products->avg('latestPrice.current');
         }
-        //for all shops, calculate the total price of all products in cart, if price is not available, get group avg price
+        //for all shops, calculate the total price of all products in cart, if price is not available, get group avg price from other shops instead
         foreach ($shops as $shop) {
+            //prices of products in cart from this shop
             $shop->totalPrice = $groups->pluck('products')->flatten()->where('shop_id', $shop->id)->sum(function ($product) {
-                return $product->latestPrice->current ?? $product->group->avgPrice;
+                return $product->latestPrice->current;
             });
+            //add prices of products not available in this shop (avg price from other shops)
+            $shop->totalPrice += $groups->whereNotIn('id', $groups->pluck('products')->flatten()->where('shop_id', $shop->id)->pluck('group_id'))->sum('avgPrice');
         }
 
         return view('cart.index', [
