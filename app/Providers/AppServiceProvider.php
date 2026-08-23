c5ace02e87a73e0c21741826c03ee40a21389c52
mateuszkulapl <mateuszkulapl@gmail.com>
2026-08-23 20:38:57 +0200

    add Eloquent strict mode configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Database\Eloquent\Model::preventAccessingMissingAttributes(!$this->app->isProduction());
+        \Illuminate\Database\Eloquent\Model::preventSilentlyDiscardingAttributes(!$this->app->isProduction());
+        \Illuminate\Database\Eloquent\Model::preventLazyLoading(!$this->app->isProduction());
     }
 }
